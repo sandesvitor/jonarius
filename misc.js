4f01dc987c327e5788e30d4308e6acc009b7b33f
Vitor Sandes <sandesvitor@gmail.com>
2024-04-11 19:07:30 -0300

    updated

--- misc.js
+++ misc.js
@@ -5,13 +5,12 @@
 // const e = [1,2,3]
 // const f = {message: "hello"}
 
-// // console.log(typeof a)
-// // console.log(typeof b)
-// // console.log(typeof c)
-// // console.log(typeof d)
-// // console.log(typeof e)
-// // console.log(typeof f)
-
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof f)
 
 // const newList = e.forEach((element) => {
 //     console.log(element)
@@ -19,27 +18,29 @@
 
 // console.log(newList)
 
-
-// const double = (x) => {
-//     return 2*x; 
-// }
-
-// const doubleImplicitReturn = (x) => 2*x;
-
-
-// console.log(double(2));
-
-// console.log("nova funcao " + doubleImplicitReturn(2));
-
 const listaDeDicionarios = [
     {id: 1, name: "Jonas"}, // key-value pair
     {id: 2, name: "Vitor"},
     {id: 3, name: "Helio"},
 ]
 
-const jonas = listaDeDicionarios.find((user, index) => {
-    console.log(index);
-    return user.name === "Jonas"
+const jonas = listaDeDicionarios.find((user) => {
+    return user.name === "Helio"
 })
 
 console.log(jonas)
+
+const findCustom = (element) => {
+    if (element.id === 2){
+        return element
+    }
+
+    return undefined
+}
+
+
+for (let i = 0; i < listaDeDicionarios.length; i++){
+    let user = findCustom(listaDeDicionarios[i])
+    console.log(user)
+}
+
